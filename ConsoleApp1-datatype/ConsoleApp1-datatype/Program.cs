49abf515f0d9f587a0fb3e254f5d42d1099f92ec
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-03-15 16:03:13 +0800

    :tada: initial commit

---
+++ ConsoleApp1-datatype/ConsoleApp1-datatype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1_datatype
+{
+    class Program
+    {
+        struct Student
+        {
+            public string name;
+            public int age;
+
+            public override string ToString()
+            {
+                return $"{name} - {age}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hewwo world");
+            //Console.ReadLine();   
+
+            Student student1;
+            student1.name = "Fareez";
+            student1.age = 12;
+
+            Console.WriteLine(student1);
+        }
+    }
+}
