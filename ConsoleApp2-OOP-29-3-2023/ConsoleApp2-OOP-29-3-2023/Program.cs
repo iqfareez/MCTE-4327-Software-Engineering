57de8d0c3cd4c5ae5e50e0baa17c5e5ae0a3aa76
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-03-29 12:19:27 +0800

    🎉 feat(ConsoleApp2-OOP-29-3-2023): add new project and files for ConsoleApp2-OOP-29-3-2023, with Vector class and Program.cs file
    🎉 feat(Vector.cs): add Vector class with x, y properties and Sum property

---
+++ ConsoleApp2-OOP-29-3-2023/ConsoleApp2-OOP-29-3-2023/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2_OOP_29_3_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2, 6);
+
+            //v1.x = 10; // error
+
+            Console.WriteLine(v1.x);
+            Console.WriteLine(v1.Sum);
+        }
+    }
+}
---
+++ ConsoleApp2-OOP-29-3-2023/ConsoleApp2-OOP-29-3-2023/Vector.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2_OOP_29_3_2023
+{
+    class Vector
+    {
+        public int x { get; } // become readonly (Encapsulation)
+        public int y { get; }
+
+        public int Sum
+        {
+            // convert method to property
+            get { return x + y; }
+        }
+
+        public Vector(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }        
+    }
+}
