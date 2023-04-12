cd06c4a34d60e38f984a1481da57cc05baeab358
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-04-12 12:12:21 +0800

    :wrench: Added exercises

---
+++ ConsoleApplication1-Birthday-Exercise/ConsoleApplication1-Birthday-Exercise/Birthday.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApplication1_Birthday_Exercise
+{
+    public class Birthday
+    {
+        public event EventHandler ImpossibleAgeEntered;
+        public event EventHandler ImproperDateEntered;
+        public event EventHandler ImproperInputEntered;
+        public event Action TodayIsBirthday;
+        public event Action<int, string> EverythingIsOkay;
+
+        public void Start(string userInput)
+        {
+            var date = userInput.Split('/');
+
+            try
+            {
+                var day = int.Parse(date[0]);
+                var month = int.Parse(date[1]);
+                var year = int.Parse(date[2]);
+
+                DateTime birthday = new DateTime(year, month, day);
+                int age = DateTime.Now.Year - birthday.Year;
+
+                if (age > 135 || age == 0)
+                {
+                    ImpossibleAgeEntered?.Invoke(this, EventArgs.Empty);
+                }
+                else
+                {
+                    EverythingIsOkay?.Invoke(age, birthday.DayOfWeek.ToString());
+
+                    if (birthday.Day == DateTime.Now.Day && birthday.Month == DateTime.Now.Month)
+                    {
+                        TodayIsBirthday?.Invoke();
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                ImproperDateEntered?.Invoke(this, EventArgs.Empty);
+            }
+            catch (FormatException)
+            {
+                ImproperInputEntered.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1-Birthday-Exercise/ConsoleApplication1-Birthday-Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication1_Birthday_Exercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var birthday = new Birthday();
+            Console.WriteLine("Enter your birthday dd/mm/yyyy" );
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input);
+            birthday.TodayIsBirthday += () => Console.WriteLine("Happy Birthday!");
+            birthday.ImpossibleAgeEntered += (sender, eventArgs) => Console.WriteLine("Impossible age entered");
+            birthday.ImproperDateEntered += (sender, eventArgs) => Console.WriteLine("Improper date entered");
+            birthday.ImproperInputEntered += (sender, eventArgs) => Console.WriteLine("Improper input entered");
+            birthday.EverythingIsOkay += (age, dayOfWeek) => Console.WriteLine($"Your age is {age} and your birthday is on {dayOfWeek}");
+            birthday.Start(input);
+        }
+    }
+}
