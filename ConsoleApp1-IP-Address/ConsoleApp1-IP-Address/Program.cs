87f09652af7cac356058186690ddaed45dfacdd2
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-05-24 11:22:42 +0800

    📡 Today's class: Netwroking

---
+++ ConsoleApp1-IP-Address/ConsoleApp1-IP-Address/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_IP_Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress a1 = IPAddress.Any;
+            Console.WriteLine(a1); // 0.0.0.0
+
+            IPAddress a2 = IPAddress.Loopback;
+            Console.WriteLine(a2); // 127.0.0.1
+
+            IPEndPoint endpoint = new IPEndPoint(a2, 80);
+            Console.WriteLine(endpoint);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp2-TCP/ConsoleApp3-TCP-Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpConnect();
+        }
+
+        static async void TcpConnect()
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, 13);
+            TcpListener listener = new TcpListener(ipEndPoint);
+
+            try
+            {
+                listener.Start();
+
+                TcpClient handler = await listener.AcceptTcpClientAsync();
+                await using NetworkStream stream = handler.GetStream();
+
+                var message = $"📅 {DateTime.Now} 🕛";
+                var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(dateTimeBytes);
+
+                Console.WriteLine($"Sent message: \"{message}\"");
+                // Sample output:
+                //     Sent message: "📅 8/22/2022 9:07:17 AM 🕛"
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
